09e9de07f44ba5732c3aa7636dfbf2d5e28a2887
pjpatel50 <pjpatel50@myseneca.ca>
2023-06-13 23:31:15 -0400

    added files

---
+++ lab1.py
@@ -0,0 +1,14 @@
+def calculate_age():
+    n = input("Enter name: ")
+    yob = int(input("Enter year of birth: "))
+    cy = 2023  
+    age = cy - yob
+    print(f"{n}, your age is {age}.")
+
+calculate_age()
+
+
+def helloWorld():
+    print('Hello World')
+
+helloWorld()
