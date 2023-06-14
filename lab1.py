8714d5c5ad00f0c495fc75d5f17a4ec476e59a21
pjpatel50 <pjpatel50@myseneca.ca>
2023-06-13 23:34:17 -0400

    Add try-except statement for TypeError in calculate_age function

--- lab1.py
+++ lab1.py
@@ -1,14 +1,11 @@
 def calculate_age():
-    n = input("Enter name: ")
-    yob = int(input("Enter year of birth: "))
-    cy = 2023  
-    age = cy - yob
-    print(f"{n}, your age is {age}.")
+    try:
+        n = input("Enter your name: ")
+        yob = int(input("Enter your year of birth: "))
+        cy = 2023 
+        age = cy - yob
+        print(f"{n}, your age is {age}.")
+    except TypeError:
+        print("Please enter an integer ")
 
 calculate_age()
-
-
-def helloWorld():
-    print('Hello World')
-
-helloWorld()
